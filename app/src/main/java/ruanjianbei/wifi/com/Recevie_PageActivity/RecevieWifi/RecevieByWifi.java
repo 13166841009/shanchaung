07a0559b6dc5e73007e0a56f3654cc424cdb76eb
749931552 <749931552@qq.com>
2016-04-28 14:53:10 +0800

    新增传递功能界面的添加----张行

---
+++ app/src/main/java/ruanjianbei/wifi/com/Recevie_PageActivity/RecevieWifi/RecevieByWifi.java
@@ -0,0 +1,15 @@
+package ruanjianbei.wifi.com.Recevie_PageActivity.RecevieWifi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ruanjianbei.wifi.com.shanchuang.R;
+
+public class RecevieByWifi extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recevie_wifi);
+    }
+}
