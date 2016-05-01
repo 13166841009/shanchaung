7a658a8156464c49c7e6cf6f7fb64384b2593d1f
linankun <764687913@qq.com>
2016-05-01 15:04:15 +0800

    Rejected commit: Default

---
+++ app/src/main/java/ruanjianbei/wifi/com/Phone_P_3G/aboutActivity.java
@@ -0,0 +1,17 @@
+package ruanjianbei.wifi.com.Phone_P_3G;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ruanjianbei.wifi.com.shanchuang.R;
+
+/**
+ * Created by linankun1 on 2016/5/1.
+ */
+public class aboutActivity extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
