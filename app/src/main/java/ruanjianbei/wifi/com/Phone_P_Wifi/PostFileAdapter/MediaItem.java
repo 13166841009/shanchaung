8a9d424ed2bc7c34f4d38f3a20c0be6883c5604b
Mr.zhou <13166841009@163.com>
2016-05-10 16:26:05 +0800

    zhouyonglong

--- app/src/main/java/ruanjianbei/wifi/com/Phone_P_Wifi/PostFileAdapter/MediaItem.java
+++ app/src/main/java/ruanjianbei/wifi/com/Phone_P_Wifi/PostFileAdapter/MediaItem.java
@@ -2,6 +2,11 @@ package ruanjianbei.wifi.com.Phone_P_Wifi.PostFileAdapter;
 
 import android.provider.MediaStore;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Properties;
 
@@ -30,6 +35,7 @@ public class MediaItem {
 	public String mData;
 	public String mTitle;
 	public String mMimeType;
+	public String size;
 
 	public MediaItem(Properties properties) {
 		try {
@@ -56,6 +62,16 @@ public class MediaItem {
 		mData = testlist.get(i);
 		mTitle = getTitle(mData);
 		mMimeType = getType(mData);
+
+
+		File ff = new File(mData);
+
+		try {
+			size = FormentFileSize(getFileSizes(ff));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 	/**
 	 * 获取文件名
@@ -78,5 +94,53 @@ public class MediaItem {
 		String type;
 		type = path.substring(path.lastIndexOf(".")+1);
 		return type;
+	}
+
+	public long getFileSizes(File f) throws IOException {
+		long s = 0;
+		if (f.exists()) {
+			FileInputStream fis = null;
+			fis = new FileInputStream(f);
+			s = fis.available();
+			fis.close();
+		} else {
+			f.createNewFile();
+			System.out.println("文件夹不存在");
+		}
+		return s;
+	}
+
+//	/**
+//	 * 递归
+//	 * */
+//	public long getFileSize(File f) {
+//		long size = 0;
+//		File flist[] = f.listFiles();
+//		for (int i = 0; i < flist.length; i++) {
+//			if (flist[i].isDirectory()) {
+//				size = size + getFileSize(flist[i]);
+//			} else {
+//				size = size + flist[i].length();
+//			}
+//		}
+//		return size;
+//	}
+
+	/**
+	 * 转换文件大小
+	 **/
+	public String FormentFileSize(long fileS) {
+		DecimalFormat df = new DecimalFormat("#.00");
+		String fileSizeString = "";
+		if (fileS < 1024) {
+			fileSizeString = df.format((double) fileS) + "B";
+		} else if (fileS < 1048576) {
+			fileSizeString = df.format((double) fileS / 1024) + "K";
+		} else if (fileS < 1073741824) {
+			fileSizeString = df.format((double) fileS / 1048576) + "M";
+		} else {
+			fileSizeString = df.format((double) fileS / 1073741824) + "G";
+		}
+		return fileSizeString;
 	}
 }
