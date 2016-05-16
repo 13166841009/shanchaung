ea810f7b5453bcb4474f5cb49057618f57c0caa1
linankun <764687913@qq.com>
2016-05-16 21:25:01 +0800

    更新了登录时推送 文件消息dianji跳转

--- app/src/main/java/activity/MyReciver.java
+++ app/src/main/java/activity/MyReciver.java
@@ -7,19 +7,34 @@ import android.os.Bundle;
 import android.util.Log;
 
 import cn.jpush.android.api.JPushInterface;
+import ruanjianbei.wifi.com.Phone_P_3G.download.downloadActivity;
 
 /**
  * Created by linankun1 on 2016/5/15.
  */
 public class MyReciver extends BroadcastReceiver {
+    private static final String TAG = "JPush";
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(JPushInterface.ACTION_MESSAGE_RECEIVED)){
-            Bundle bundle = intent.getExtras();
-            String title = bundle.getString(JPushInterface.EXTRA_TITLE);
-            String message = bundle.getString(JPushInterface.EXTRA_MESSAGE);
-//                Toast.makeText(context, "Message Title:"+title+" Content:"+message, Toast.LENGTH_SHORT).show();
-            Log.i("biaoshi:",title+" "+message);
+        Bundle bundle = intent.getExtras();
+        if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
+            String regId = bundle.getString(JPushInterface.EXTRA_REGISTRATION_ID);
+            Log.d(TAG, "[MyReceiver] 接收Registration Id : " + regId);
+        } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
+            System.out.println("收到了自定义消息。消息内容是：" + bundle.getString(JPushInterface.EXTRA_MESSAGE));
+            // 自定义消息不会展示在通知栏，完全要开发者写代码去处理
+        } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
+            System.out.println("收到了通知");
+            // 在这里可以做些统计，或者做些其他工作
+        } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
+            System.out.println("用户点击打开了通知");
+            // 在这里可以自己写代码去定义用户点击后的行为
+            Intent i = new Intent(context, downloadActivity.class);  //自定义打开的界面
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+
+        } else {
+            Log.d(TAG, "Unhandled intent - " + intent.getAction());
         }
     }
 }
